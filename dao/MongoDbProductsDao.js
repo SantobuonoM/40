966754bc0839c547b13757b0abdf2dfcecdd3f66
Matias <matiasantobuono>
2023-01-02 15:35:18 -0300

    fixing

--- dao/MongoDbProductsDao.js
+++ dao/MongoDbProductsDao.js
@@ -1,10 +1,17 @@
 import ContenedorMongoDB from "../container/MongoDbContainer.js";
 import ProductoModel from "../models/productos.model.js";
-
+let instance = [];
 class ProductoDAOMongoDB extends ContenedorMongoDB {
-    constructor(){
-        super(ProductoModel);
+  constructor() {
+    super(ProductoModel);
+  }
+  static getInstance() {
+    if (!instance) {
+      instance = new ProductoDAOMongoDB();
     }
+
+    return instance;
+  }
 }
 
 export default ProductoDAOMongoDB;
